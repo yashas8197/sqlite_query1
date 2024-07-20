404371918301c06606b51f5d475f0730a94d565b
Yashas V <130334248+yashas8197@users.noreply.github.com>
2024-07-20 05:44:36 +0000

    add: filters

--- index.js
+++ index.js
@@ -104,6 +104,46 @@ app.get("/movies/release_year/:year", async (req, res) => {
   }
 });
 
+async function filterByActor(actor) {
+  let query = "SELECT * FROM movies WHERE actor = ?";
+  let response = await db.all(query, [actor]);
+  return { movies: response };
+}
+
+app.get("/movies/actor/:actor", async (req, res) => {
+  try {
+    let actor = req.params.actor;
+    let result = await filterByActor(actor);
+
+    if (result.movies.length === 0) {
+      return res.status(404).json({ message: "No Movie Found" });
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+async function filterByDirector(director) {
+  let query = "SELECT * FROM movies WHERE director = ?";
+  let response = await db.all(query, [director]);
+  return { movies: response };
+}
+
+app.get("/movies/director/:director", async (req, res) => {
+  try {
+    let director = req.params.director;
+    let result = await filterByDirector(director);
+
+    if (result.movies.length === 0) {
+      return res.status(404).json({ message: "No Movie Found" });
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
